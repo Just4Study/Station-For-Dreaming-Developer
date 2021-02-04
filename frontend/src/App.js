e6a0427bda24a78486e24ae4325b56495ca5f431
hughugh <lsjppp8069@kookmin.ac.kr>
2021-02-05 03:57:48 +0900

    add main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
-import Home from './pages/Home'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
+import MainPage from './pages/MainPage'
+import Header from './layouts/Header'
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Route exact path='/' component={Home}/>
+      <Route path='/' component={Header}/>
+      <Route exact path='/' component={MainPage}/>
       <Route exact path='/signup' component={SignUp}/>
       <Route exact path='/signin' component={SignIn}/>
+      <Route path='/mainPage' component={MainPage}/>
     </BrowserRouter>
   )
 }
