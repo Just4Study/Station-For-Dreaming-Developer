7fbea401d8889ce96d2cea732d5a2ee7c0edbf30
hughugh <lsjppp8069@kookmin.ac.kr>
2021-02-04 16:40:18 +0900

    remove unuses

--- frontend/src/stores/app.js
+++ frontend/src/stores/app.js
@@ -2,11 +2,11 @@ import { observable } from 'mobx'
 
 const createStore = () => {
   const appStore = {
-    // test: observable.box(null)
+    test: observable.box(null),
 
-    // chageTest(data) {
-    //   appStore.test.set(data)
-    // }
+    chageTest(data) {
+      appStore.test.set(data)
+    },
   }
 
   return appStore
