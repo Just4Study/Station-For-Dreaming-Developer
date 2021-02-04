85913d7319ed6b75c5bcd8c0fed6a26bcaec66a7
Just4Study <Just4Study@g.skku.edu>
2021-02-04 19:07:22 +0900

    DB Model Upload

--- backend/post/models.py
+++ backend/post/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+class Post(models.Model):
+    title = models.CharField(max_length=200, blank = True)
+    author = models.CharField(max_length=20, blank = True)
+    team_type = models.CharField(max_length=20)
+    region = models.CharField(max_length= 10)
+    end_date = models.DateField(null = True)
+    dev_category = models.CharField(max_length = 10)
+    comment = models.TextField()
+
+    def __str__(self):
+        return self.title
+
 # Create your models here.
