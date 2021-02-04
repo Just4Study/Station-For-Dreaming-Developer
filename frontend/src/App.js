8e3c12a52cecc15be2d636f259c10a8ab75a31bf
JK Park <kuro11pow2@gmail.com>
2021-02-04 23:34:44 +0900

    SignIn Page Upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
 import SignUp from './pages/SignUp'
-
+import SignIn from './pages/SignIn'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Route exact path='/' component={Home}/>
       <Route exact path='/signup' component={SignUp}/>
+      <Route exact path='/signin' component={SignIn}/>
     </BrowserRouter>
   )
 }
--- frontend/src/pages/SignIn/index.jsx
+++ frontend/src/pages/SignIn/index.jsx
@@ -1,13 +1,51 @@
-import React, { useState, useEffect } from 'react'
-import { Link, useHistory } from 'react-router-dom'
+import React, { useState } from 'react'
 
-import styles from './styles.css'
+import signinstyle from './signin.module.css'
+import logo from '../../imgs/logo.png'
 
 
 const App = () => {
+  const styles = signinstyle
+  const [infos, setInfos] = useState({
+    email: '',
+    password: '',
+  })
+
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setInfos(infos => ({...infos, [name]: value}));
+  }
+
   return (
-    <div>
-      Home Page
+    <div className={styles.container}>
+      <div className={styles.signInContainer}>
+        <img
+          src={logo}
+          alt='logo'
+          className={styles.logoContainer}
+        />
+        <input
+          className={styles.inputBox}
+          placeholder='EMAIL'
+          name='email'
+          value={infos['email']}
+          onChange={(e) => onChangeHandler(e)}
+        />
+        <input
+          className={styles.inputBox}
+          placeholder='PASSWORD'
+          name='password'
+          value={infos['password']}
+          onChange={(e) => onChangeHandler(e)}
+        />
+        <div className={styles.signUpBtnContainer}>
+          <button
+            className={styles.signUpBtn}
+          >
+            SIGN IN
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
