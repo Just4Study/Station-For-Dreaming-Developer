841e08f886c35bff5536b5610b7f2c14d971396c
hughugh <lsjppp8069@kookmin.ac.kr>
2021-02-06 01:02:32 +0900

    post logic

--- frontend/src/pages/MainPage/index.jsx
+++ frontend/src/pages/MainPage/index.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 import axios from 'axios'
@@ -74,7 +73,7 @@ const App = () => {
   useEffect(() => {
     let url = 'http://localhost:8000/post?';
     if(currType !== '' && currCategory !== '') {
-      onChangeHandler(url + 'team_type=' + currType + '&' + 'dev_category=' + currCategory)
+      onChangeHandler(url + 'team_type=' + currType + '&dev_category=' + currCategory)
     } else if(currType === '' && currCategory !== '') {
       onChangeHandler(url + 'dev_category=' + currCategory)
     } else if(currType !== '' && currCategory === '') {
