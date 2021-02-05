91bb163c901635f1cf7e02a54703075aac4c4605
Just4Study <pidaoh@g.skku.edu>
2021-02-05 15:56:41 +0900

    BackEnd Model Update

--- backend/post/serializers.py
+++ backend/post/serializers.py
@@ -4,4 +4,4 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ('auto_id','title', 'author', 'team_type', 'region', 'end_date', 'dev_category', 'comment')
+        fields = ('auto_id','title', 'author', 'team_type', 'region', 'end_date', 'dev_category', 'preference','comment')
